8e1ed264133833f7fd4f0150548b524691358dd2
kuma <contact@sanozi.com>
2022-03-11 14:18:48 +0900

    francに関しても同様にMoneyへキャストを行う

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -10,7 +10,7 @@ public class Franc extends Money {
     }
 
     public boolean equals(Object object) {
-        Franc franc = (Franc)object;
-        return amount == franc.amount;
+        Money money = (Money)object;
+        return amount == money.amount;
     }
 }
